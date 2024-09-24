bad6744b00b340b6db4b828ad31da148a9b78a2f
jbmdevelopment <jbarreramartinez100@student.hbuhsd.edu>
2024-09-24 10:47:14 -0700

    Update WarmUp.java

--- WarmUp.java
+++ WarmUp.java
@@ -1,7 +1,15 @@
 public class WarmUp
 {
+  public static boolean isEven(int N) {
+     return (N % 2 == 0);
+  }
+  public static boolean isDivisible(int x, int y) {
+    return (x % y == 0);
+  }
   public static void main(String[] args)
   {
+    System.out.println(isEven(2));
+    System.out.println(isDivisible(6, 3));
     // test your methods here
   }
 
